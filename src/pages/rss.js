5b411437e26ac041b3b4b3f807d17a199ccfe1cd
devesh/webdunia <devesh.sant@webdunia.net>
2021-03-24 20:53:36 +0530

    rss updated

--- src/pages/rss.js
+++ src/pages/rss.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import axios from 'axios';
-import XMLViewer from 'react-xml-viewer';
+//import axios from 'axios';
+//import XMLViewer from 'react-xml-viewer';
 export default class RSS extends React.Component {
    
-    constructor(props) {
-        super(props);
-        this.state = {
-           data : {}
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //        data : {}
 
-         };
-      }
+    //      };
+    //   }
 
       componentDidMount() {
             // const url= Globals.language_based_api+'/home-page';
@@ -23,19 +23,19 @@ export default class RSS extends React.Component {
             // }) 
 
             // const XMLData = "https://hindi.webdunia.com/rss/hindi-news-101.rss"; 
-            const XMLData = "http://wsdev.webdunia.com/json/test-rss/get-rss?lang=hi"; 
+            // const XMLData = "http://wsdev.webdunia.com/json/test-rss/get-rss?lang=hi"; 
 
-            axios.get(XMLData, {
-               "Content-Type": "application/xml; charset=utf-8"
-            })
-            .then((response) => {
-                this.setState({
-                    data : response
+            // axios.get(XMLData, {
+            //    "Content-Type": "application/xml; charset=utf-8"
+            // })
+            // .then((response) => {
+            //     this.setState({
+            //         data : response
                    
-                  })
+            //       })
                  
 
-            });
+            // });
 
           }
     
@@ -46,16 +46,16 @@ export default class RSS extends React.Component {
        // console.log("🚀 ~ file: rss.js ~ line 41 ~ RSS ~ render ~ myHeaders", myHeaders)
        // var XMLHttpRequest = new XMLHttpRequest();
        // XMLHttpRequest.setRequestHeader("content-type", "application/xml")
-        console.log("rss",this.state.data)
+        // console.log("rss",this.state.data)
        
-        let XMLfinal = this.state.data;
-        console.log("🚀 ~ file: rss.js ~ line 49 ~ RSS ~ render ~ XMLfinal", XMLfinal.data )
+        // let XMLfinal = this.state.data;
+        // console.log("🚀 ~ file: rss.js ~ line 49 ~ RSS ~ render ~ XMLfinal", XMLfinal.data )
         //let filter = XMLfinal.replace(/ /g, '');
         
        // console.log("🚀 ~ file: rss.js ~ line 48 ~ RSS ~ render ~ data", data)
         return (
             
-                 <XMLViewer string={XMLfinal.data} /> 
+                 <div></div>
            
         )
     }
